c30e537d240d77a4dbe13d187a0ae923f30e4482
Jonathan Striblet <jstriblet@gmail.com>
2020-06-04 21:09:24 -0700

    only check the paragraph

--- CheckSite.sh
+++ CheckSite.sh
@@ -4,12 +4,17 @@ cd ~/Documents/CS/SFEventsChange
 
 wget -o Log-File -O one-hour-weddings-curr http://sfcityhallevents.org/one-hour-weddings
 
-file1=`cat one-hour-weddings-curr`
-file2=`cat one-hour-weddings-prev`
+#file1=`cat one-hour-weddings-curr`
+#file2=`cat one-hour-weddings-prev`
 
-if [ "$file1" = "$file2" ]; then
-	#echo "files are equal"
+p1=`head -n 724 one-hour-weddings-curr | tail -n 9`
+p2=`head -n 724 one-hour-weddings-prev | tail -n 9`
+
+diff=`sdiff -s <(echo $p1) <(echo $p2)`
+
+if [ "$p1" = "$p2" ]; then
+	echo "Paragraphs are the same."
 else
-  cat Notification-Email | /usr/sbin/sendmail -t
-	#echo "files are not equal"
+	cat Notification-Email | /usr/sbin/sendmail -t
+	echo "Paragraphs are not the same."
 fi
